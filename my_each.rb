6c5f3ffb83624368b2ae87fd7aa729bb820a4859
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 05:22:01 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,28 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (words)        # put argument(s) here
+    words = ['hi', 'hello', 'bye', 'goodbye']
+    i = 0
+    while i <= words.length
+     i = [i]
+      puts 
+      i += 1 
+    yield 
+      puts 
+    end
+end
+
+
+def my_other_each(tas)
+  tas = ['arel', 'jon', 'logan', 'spencer']
+  tas.length
+  my_other_each(tas) do |ta|
+     times_called += 1
+  if tas.kind_of?(String)
+    puts true
+  else 
+    puts false 
+  end
+end
+
+def my_proc.new
+  return Proc.new {|n| n*tas}
 end
